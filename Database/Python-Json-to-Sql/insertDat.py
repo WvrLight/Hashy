dcd14b73c157a6d22d03ec491ea06bcd7172c3a9
Asher <51806917+asher-lab@users.noreply.github.com>
2021-02-03 20:19:38 +0800

    Update insertDat.py
    insert data into table named experiments1 -> on database.db

--- Database/Python-Json-to-Sql/insertDat.py
+++ Database/Python-Json-to-Sql/insertDat.py
@@ -3,9 +3,11 @@
 
 #inserting values into tables, adding values into tables
 
+#https://www.sitepoint.com/getting-started-sqlite3-basic-commands/
+
 import sqlite3
 
-conn = sqlite3.connect('insert.db')
+conn = sqlite3.connect('database.db')
 cur = conn.cursor()
 cur.execute('CREATE TABLE experiments1 (name VARCHAR, description VARCHAR)')
 conn.commit()
@@ -13,10 +15,13 @@ conn.commit()
 #conn.close()
 
 #Adding data into a database
-cur.execute('INSERT INTO experiments1 (name, description) values ("Aquiles", "My experiment description")')
+cur.execute('INSERT INTO experiments1 ( name, email, website_url, comment ) VALUES ( 'Shivam Mamgain', 'xyz@gmail.com', 'shivammg.blogspot.com', 'Great tutorial for beginners.')')
 conn.commit()
+#Adding data into a database
+#cur.execute('INSERT INTO experiments1 (name, description) values ("Aquiles", "My experiment description")')
+#conn.commit()
 
-cur.execute('INSERT INTO experiments1 (name, description) VALUES (?, ?)',
+#cur.execute('INSERT INTO experiments1 (name, description) VALUES (?, ?)',
             ('Another User', 'Another Experiment, even using " other characters"'))
-conn.commit()
+#conn.commit()
 
