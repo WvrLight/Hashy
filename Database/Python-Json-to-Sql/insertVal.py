ef0520b2fdc3c85a69b6984c82a4bade286e2496
Asher <51806917+asher-lab@users.noreply.github.com>
2021-02-03 20:32:43 +0800

    Update and rename insertDat.py to insertVal.py
    corrected all code:
    
    What does this code do:
    - create a table named experiments1 then put this into database.db
    - insert values to experiments1

--- Database/Python-Json-to-Sql/insertVal.py
+++ Database/Python-Json-to-Sql/insertVal.py
@@ -9,19 +9,21 @@ import sqlite3
 
 conn = sqlite3.connect('database.db')
 cur = conn.cursor()
-cur.execute('CREATE TABLE experiments1 (name VARCHAR, description VARCHAR)')
+#cur.execute('CREATE TABLE experiments1 (name VARCHAR, description VARCHAR)')
+cur.execute('CREATE TABLE experiments1 (post_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, website_url TEXT NULL, comment TEXT NOT NULL )')
+
 conn.commit()
 
 #conn.close()
 
 #Adding data into a database
-cur.execute('INSERT INTO experiments1 ( name, email, website_url, comment ) VALUES ( 'Shivam Mamgain', 'xyz@gmail.com', 'shivammg.blogspot.com', 'Great tutorial for beginners.')')
+cur.execute('INSERT INTO experiments1 ( name, email, website_url, comment ) VALUES ( "Shivam Mamgain", "xyz@gmail.com", "shivammg.blogspot.com", "Great tutorial for beginners.")')
 conn.commit()
 #Adding data into a database
 #cur.execute('INSERT INTO experiments1 (name, description) values ("Aquiles", "My experiment description")')
 #conn.commit()
 
 #cur.execute('INSERT INTO experiments1 (name, description) VALUES (?, ?)',
-            ('Another User', 'Another Experiment, even using " other characters"'))
+#            ('Another User', 'Another Experiment, even using " other characters"'))
 #conn.commit()
 
