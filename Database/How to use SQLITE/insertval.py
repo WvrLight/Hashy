ed4e328113a3e1103ad3e58b8366abb7069236d7
Asher <51806917+asher-lab@users.noreply.github.com>
2021-02-03 18:55:37 +0800

    Create insertval.py

---
+++ Database/How to use SQLITE/insertval.py
@@ -0,0 +1,19 @@
+#inserting values into tables, adding values into tables
+
+import sqlite3
+
+conn = sqlite3.connect('insert.db')
+cur = conn.cursor()
+cur.execute('CREATE TABLE experiments1 (name VARCHAR, description VARCHAR)')
+conn.commit()
+
+#conn.close()
+
+#Adding data into a database
+cur.execute('INSERT INTO experiments1 (name, description) values ("Aquiles", "My experiment description")')
+conn.commit()
+
+cur.execute('INSERT INTO experiments1 (name, description) VALUES (?, ?)',
+            ('Another User', 'Another Experiment, even using " other characters"'))
+conn.commit()
+
